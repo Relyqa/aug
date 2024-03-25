2f7279b3cc14b99d3143b9795e352de2ae4dbc55
Manu <m.gutierrez@sialitech.com>
2024-03-25 09:51:50 +0100

    test

---
+++ test.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+def argumentos():
+    """Parse execution arguments.
+    """
+    parser = argparse.ArgumentParser(description='YOLOv8 training parameters')
+    parser.add_argument('--base_model', type=str, help='yolov8 base model ')
+    args = parser.parse_args()
+    return args
+
+
+def main(base_model):
+    print("Inicio")
+    print(base_model)
+    print("Final")
+
+
+if __name__ == '__main__':
+    args = argumentos()
+    main(args.base_model)
